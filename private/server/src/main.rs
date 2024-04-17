3bce86de688411008335153eba75f05e4df42165
miguel <mi.arevalo@proton.me>
2024-04-17 16:25:17 -0400

    Wrote some toy functions to make sure rust and the framework are working.

--- private/server/src/main.rs
+++ private/server/src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use] extern crate rocket;
+
+use std::env;
+
+#[get("/")]
+fn index() -> &'static str {
+
+    if let Ok(curr_dir) = env::current_dir() {
+        println!("current directory: {:?}", curr_dir);
+    }
+
+    return "hello world!";
+}
+
+#[get("/world")]
+fn world() -> &'static str {
+    "Hello, world!"
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![index, world])
 }
