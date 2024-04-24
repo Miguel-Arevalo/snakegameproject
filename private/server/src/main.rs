8da58ee21f587b9e737893ab7e64e329a516ab48
miguel <mi.arevalo@proton.me>
2024-04-24 07:09:20 -0400

    skeleton server for sending score to server

--- private/server/src/main.rs
+++ private/server/src/main.rs
@@ -1,9 +1,16 @@
 #[macro_use] extern crate rocket;
 
 use rocket::fs::{FileServer, relative};
+use rocket::response::status;
+
+#[post("/")]
+fn score() -> status::Accepted<()> {
+    status::Accepted(())
+}
 
 
 #[launch]
 fn rocket() -> _ {
     rocket::build().mount("/",  FileServer::from(relative!("../../public")))
+    .mount("/", routes![score])
 }
--- public/main.js
+++ public/main.js
@@ -1,12 +1,20 @@
 
 import {run_game} from "./snake_game.js";
 
-  function game_wrapper() {
+
+function game_wrapper() {
 
     function game_over(e) {
         console.log(`final score: ${e.detail}`);
 
-        
+        let data = { score: e.detail }
+
+        fetch("https://snakegameproject.online", {
+            method: 'POST',
+            headers: { "Content-Type": "application/json"},
+            body: JSON.stringify(data),
+        }).then(() => console.log("score sent!"));
+
     }
 
     document.addEventListener("game_over", game_over);
